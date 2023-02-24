d623b390af0a9b87d025512542ae7bf1303dabe4
Prima Interaktif - Developer <ahmdfatihin17@gmail.com>
2023-02-24 14:45:12 +0700

    feat: create navbar

--- lib/module/home/screens/navbar_screen.dart
+++ lib/module/home/screens/navbar_screen.dart
@@ -1,11 +1,65 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:furniture_shop/const/colors.dart';
+import 'package:furniture_shop/gen/assets.gen.dart';
 
-class NavbarScreen extends StatelessWidget {
+import '../../../routes/routes.dart';
+import '../providers/navbar_provider.dart';
+
+class NavbarScreen extends ConsumerWidget {
   const NavbarScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return AutoTabsScaffold(
+        extendBody: true,
+        routes: const [
+          HomeMenu(),
+          CartMenu(),
+          WishlistMenu(),
+          ProfileMenu(),
+        ],
+        bottomNavigationBuilder: (_, tabsRouter) {
+          return Container(
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                  color: grey,
+                  width: 0.5,
+                ),
+              ),
+            ),
+            child: BottomNavigationBar(
+              iconSize: 30,
+              elevation: 0,
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: white,
+              showUnselectedLabels: true,
+              selectedItemColor: red,
+              unselectedItemColor: grey,
+              currentIndex: tabsRouter.activeIndex,
+              onTap: tabsRouter.setActiveIndex,
+              items: [
+                BottomNavigationBarItem(
+                  label: '',
+                  icon: ImageIcon(AssetImage(Assets.icons.navbarHome.path)),
+                ),
+                BottomNavigationBarItem(
+                    label: '',
+                    icon: ImageIcon(
+                      AssetImage(Assets.icons.navbarCart.path),
+                    )),
+                BottomNavigationBarItem(
+                    label: '',
+                    icon: ImageIcon(AssetImage(Assets.icons.navbarStar.path))),
+                BottomNavigationBarItem(
+                    label: '',
+                    icon:
+                        ImageIcon(AssetImage(Assets.icons.navbarPerson.path))),
+              ],
+            ),
+          );
+        });
   }
 }
