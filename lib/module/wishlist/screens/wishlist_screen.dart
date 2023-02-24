bce8ae8eda091de63d386d6e21bfd3e6a6d3f631
Prima Interaktif - Developer <ahmdfatihin17@gmail.com>
2023-02-24 14:07:00 +0700

    feaat: add cart, profile, and wishlist for navbar

---
+++ lib/module/wishlist/screens/wishlist_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WishlistScreen extends StatelessWidget {
+  const WishlistScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Cart"),
+      ),
+    );
+  }
+}
